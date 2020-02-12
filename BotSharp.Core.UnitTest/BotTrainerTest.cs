0884eaf128f2cfd4f84a2cc999d1e44395b637ed
fmoraestec <34696450+fmoraestec@users.noreply.github.com>
2020-02-11 21:25:02 -0300

    Config #1

---
+++ BotSharp.Core.UnitTest/BotTrainerTest.cs
@@ -0,0 +1,18 @@
+﻿using BotSharp.Core.Engines;
+using BotSharp.Platform.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotSharp.Core.UnitTest
+{
+    [TestClass]
+    public class BotTrainerTest : TestEssential
+    {
+        [TestMethod]
+        public async void TrainingTest()
+        {
+        }
+    }
+}

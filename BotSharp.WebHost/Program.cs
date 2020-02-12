634e01c175541dda20b7b03e402b0ce536d73057
fmoraestec <34696450+fmoraestec@users.noreply.github.com>
2020-02-11 21:33:01 -0300

    Part 5

---
+++ BotSharp.WebHost/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace BotSharp.WebHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            Microsoft.AspNetCore.WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    string dir = Path.GetFullPath(env.ContentRootPath);
+                    string settingsFolder = Path.Combine(dir, "Settings");
+
+                    // locate setting folder
+                    if (!Directory.Exists(settingsFolder))
+                    {
+                        dir = Path.GetFullPath(env.ContentRootPath + "/..");
+                    }
+
+                    settingsFolder = Path.Combine(dir, "Settings");
+
+                    if (!Directory.Exists(settingsFolder))
+                    {
+                        dir = Path.GetFullPath(env.ContentRootPath + "/bin");
+                    }
+
+                    settingsFolder = Path.Combine(dir, "Settings");
+
+                    Console.WriteLine($"Read settings from {settingsFolder}");
+
+                    var settings = Directory.GetFiles(settingsFolder, "*.json");
+                    settings.ToList().ForEach(setting =>
+                    {
+                        config.AddJsonFile(setting, optional: false, reloadOnChange: true);
+                    });
+                })
+                .UseUrls("http://0.0.0.0:3112")
+                .UseStartup<Startup>()
+                .Build();
+    }
+}

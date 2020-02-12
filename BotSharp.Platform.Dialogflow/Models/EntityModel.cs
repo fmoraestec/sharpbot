b16d335afaadf03ff9bb35b2e4801a02b4dbd0d9
fmoraestec <34696450+fmoraestec@users.noreply.github.com>
2020-02-11 21:28:23 -0300

    Part 2

---
+++ BotSharp.Platform.Dialogflow/Models/EntityModel.cs
@@ -0,0 +1,18 @@
+﻿using BotSharp.Platform.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotSharp.Platform.Dialogflow.Models
+{
+    public class EntityModel : EntityBase
+    {
+        public string Agent { get; set; }
+
+        public string EntityName { get; set; }
+
+        public string Type { get; set; }
+
+        public string UiColor { get; set; }
+    }
+}
